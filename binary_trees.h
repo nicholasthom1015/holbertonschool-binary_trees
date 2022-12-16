7d01e0bc8a733373fba2d41001f76d32b4ff8c80
nicholasthom1015 <nicholasthomas10.15@gmail.comas10.15@gmail.com>
2022-12-16 10:14:17 -0800

    binary_tree_node

---
+++ binary_trees.h
@@ -0,0 +1,31 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct binary_tree_s - binary tree node
+ * @n: interger stored in node
+ * @parent: pointer to partent node
+ * @left: pointer to left child
+ * @right pointer to right child
+ */
+
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+#endif
